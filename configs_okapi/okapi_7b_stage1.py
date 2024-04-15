9ac06f011fe9f941a6ae0e8882f3639849012548
taiyan <yan.tai@sjtu.edu.cn>
2024-04-15 15:09:16 +0800

    config debug

--- configs_okapi/okapi_7b_stage1.py
+++ configs_okapi/okapi_7b_stage1.py
@@ -4,12 +4,11 @@ from xtuner.utils import PROMPT_TEMPLATE
 
 from mmengine.config import read_base
 with read_base():
-    from _base_.models.all_tokenizers import vicuna_7b_path, vicuna_7b_path_tokenizer
-    from _base_.models.all_visual_encoders import clip_patch14_336
-    from _base_.datasets.okapi_train_stage1 import *
-    from _base_.schedules.schedule import *
-    from _base_.runtimes.okapi_vicuna_runtime import *
-    from _base_.runtimes.default_runtime import *
+    from ._base_.models.all_tokenizers import vicuna_7b_path, vicuna_7b_path_tokenizer
+    from ._base_.models.all_visual_encoders import clip_patch14_336
+    from ._base_.datasets.okapi_train_stage1 import *
+    from ._base_.schedules.schedule import *
+    from ._base_.default_runtime import *
 
 
 # Evaluate the generation performance during the training
