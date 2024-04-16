686421945813f86c66a9713512fbc0a9d1e9c1fc
wefwefWEF2 <67530674@qq.com>
2024-04-16 23:17:02 +0800

    coco

--- xtuner/dataset/single_dataset/coco_rem.py
+++ xtuner/dataset/single_dataset/coco_rem.py
@@ -4,15 +4,12 @@ from torch.utils.data import Dataset
 from pycocotools.coco import COCO
 from PIL import Image
 
-class CoCoDataset(Dataset):
+class CoCoRemDataset(Dataset):
     def __init__(self, root, annotation_file):
         """
         Args:
             root (string): Root directory where images are downloaded to.
             annotation_file (string): Path to the COCO annotation file.
-            transform (callable, optional): A function/transform that takes in an image and returns a transformed version.
-            target_transform (callable, optional): A function/transform that takes in the target and transforms it.
-            use_custom_bboxes (bool): Flag to use custom bounding boxes instead of COCO ones.
         """
         self.root = root
         self.coco = COCO(annotation_file)
@@ -26,15 +23,6 @@ class CoCoDataset(Dataset):
 
         path = coco.loadImgs(img_id)[0]['file_name']
         img = Image.open(os.path.join(self.root, path)).convert('RGB')
-
-        # Example
-        for target in target:
-            area = target['area']
-            bbox = target['bbox']
-            category_id = target['category_id']
-            image_id = target['image_id']
-            segmentation = target['segmentation']
-
         return img, target
 
     def __len__(self):
@@ -42,7 +30,9 @@ class CoCoDataset(Dataset):
 
 if __name__ == '__main__':
 
-    dataset = CoCoDataset(root='/data/Aaronzhu/DatasetStage1/MSCOCO/2017/train2017',
-                         annotation_file='/data/Aaronzhu/DatasetStage1/COCO-ReM/instances_trainrem.json')
-    data = dataset[0]
-    print(data)
+    image_path = '/data/Aaronzhu/DatasetStage1/MSCOCO/2017/train2017'
+    annotation_file = '/data/Aaronzhu/DatasetStage1/COCO-ReM/instances_trainrem.json'
+
+    dataset = CoCoRemDataset(root=image_path, annotation_file=annotation_file)
+    image, annotation = dataset[0]
+    print(image, annotation)
