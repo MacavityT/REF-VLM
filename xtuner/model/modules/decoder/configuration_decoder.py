cf07c217db31b7a963e6aad3a7f312a835c8185e
taiyan <yan.tai@sjtu.edu.cn>
2024-04-26 16:53:29 +0800

    [taiyan] deleted trash folder 'decoder '

--- xtuner/model/modules/decoder/configuration_decoder.py
+++ xtuner/model/modules/decoder/configuration_decoder.py
@@ -21,3 +21,28 @@ class DecoderConfig(PretrainedConfig):
         self.hidden_act = hidden_act
         self.bias = bias
         super().__init__(**kwargs)
+
+
+
+class ProjectorConfig(PretrainedConfig):
+    model_type = 'decoder'
+    _auto_class = 'AutoConfig'
+
+    def __init__(
+        self,
+        hidden_size=1024,
+        intermediate_size=14336,
+        num_local_experts=8,
+        num_experts_per_tok=2,
+        hidden_act="silu",
+        router_jitter_noise=0.0,
+        **kwargs,
+    ):
+        self.hidden_size = hidden_size
+        self.intermediate_size = intermediate_size
+        self.num_local_experts = num_local_experts
+        self.num_experts_per_tok = num_experts_per_tok
+        self.hidden_act = hidden_act
+        self.router_jitter_noise = router_jitter_noise
+        super().__init__(**kwargs)
+
