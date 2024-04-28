37e42aa9482df53693ba20142c54c6cd7e73c0d1
Luhaozhu <lhzhu1999@outlook.com>
2024-04-28 11:09:40 +0800

    change

--- scripts/train_dist_multi_stage1.sh
+++ scripts/train_dist_multi_stage1.sh
@@ -18,7 +18,7 @@ host5_addr=$(echo $VC_TASK1_HOSTS | awk -F, '{print $5}')
 host6_addr=$(echo $VC_TASK1_HOSTS | awk -F, '{print $6}')
 
 
-work_dir="/model/Aaronzhu/OkapiModel/llama3_8b/0426_1_20_gc_rvg"
+work_dir="/model/Aaronzhu/OkapiModel/vicuna_13b/0428_1_20_gc_rvg"
 
 local_host=$(hostname)
 echo "local host: $local_host"
