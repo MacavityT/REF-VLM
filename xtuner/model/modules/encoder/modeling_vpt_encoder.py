22cfbe8faba7231224b91663e0c05d60cd333267
taiyan <yan.tai@sjtu.edu.cn>
2024-07-16 17:05:51 +0800

    [taiyan] Added mask_token embedding in vpt_encoder

--- xtuner/model/modules/encoder/modeling_vpt_encoder.py
+++ xtuner/model/modules/encoder/modeling_vpt_encoder.py
@@ -27,6 +27,28 @@ class VPTEncoderModel(PreTrainedModel):
                 stride=config.patch_size,
                 bias=False,
             )
+
+        if config.use_projector:
+            # projector
+            modules = [
+                nn.Linear(
+                    config.visual_hidden_size,
+                    config.llm_hidden_size,
+                    bias=config.bias)
+            ]
+            for _ in range(1, config.depth):
+                modules.append(ACT2FN[config.hidden_act])
+                modules.append(
+                    nn.Linear(
+                        config.llm_hidden_size,
+                        config.llm_hidden_size,
+                        bias=config.bias))
+            self.model = nn.Sequential(*modules)
+
+        if config.use_mask_token:
+            self.mask_pooling = nn.AdaptiveAvgPool2d((1, 1))
+            self.mask_embedding = nn.Linear(1, config.visual_hidden_size)
+
         self.position_embedding = nn.Embedding(config.num_patches, config.visual_hidden_size)
         self.register_buffer("position_ids", torch.arange(config.num_patches).expand((1, -1)), persistent=False)
 
@@ -37,6 +59,8 @@ class VPTEncoderModel(PreTrainedModel):
         if self.config.strategy == 'embedding':
             self.patch_embedding.register_forward_hook(make_inputs_require_grad)
         self.position_embedding.register_forward_hook(make_inputs_require_grad)
+        if self.config.use_projector:
+            self.model.register_forward_hook(make_inputs_require_grad)
 
     def _set_gradient_checkpointing(self, module, value=False):
         if isinstance(module, VPTEncoderModel):
@@ -99,16 +123,31 @@ class VPTEncoderModel(PreTrainedModel):
             ).contiguous()
         elif self.config.strategy == 'embedding':
             target_dtype = self.patch_embedding.weight.dtype
-            x = x.unsqueeze(1) # [b, 1, c, h, w]
-            mask = mask.unsqueeze(2) # [b, q, 1, h, w]
-            vpt_feats = x * mask # [b, q, c, h, w]
+            x_expand = x.unsqueeze(1) # [b, 1, c, h, w]
+            mask_expand = mask.unsqueeze(2) # [b, q, 1, h, w]
+            vpt_feats = x_expand * mask_expand # [b, q, c, h, w]
             vpt_feats = vpt_feats.view(-1, c, h, w) # [b*q, c, h, w]
             vpt_feats = self.patch_embedding(vpt_feats.to(dtype=target_dtype)) # shape = [b*q, c, 3, 3]
             vpt_feats = vpt_feats.flatten(2).transpose(1, 2) # [b*q, c, n]->[b*q, n, c],  n=9
-            vpt_feats = vpt_feats.view(b, q, self.config.num_patches, c)
+            vpt_feats = vpt_feats.view(b, q, self.config.num_patches, c) # [b, q, n, c]
         else:
             raise NotImplementedError
         return vpt_feats
+
+    def get_mask_token(self, mask):
+        mask_feats = mask.unsqueeze(-1) # [b, q, h, w, 1]
+        mask_feats = self.mask_embedding(mask_feats) # [b, q, h, w, c]
+        mask_feats = mask_feats.permute(0, 1, 4, 2, 3) # [b, q, c, h, w]
+        mask_token = self.mask_pooling(mask_feats).squeeze(-1) # [b, q, c, 1]
+        mask_token = mask_token.permute(0, 1, 3, 2) # [b, q, 1, c]
+        return mask_token
+
+    def project(self, x):
+        if self.gradient_checkpointing and self.training:
+            layer_outputs = torch.utils.checkpoint.checkpoint(self.model, x)
+        else:
+            layer_outputs = self.model(x)
+        return layer_outputs
 
     def forward(self, x, regions, return_dict=True):
         """
@@ -129,6 +168,13 @@ class VPTEncoderModel(PreTrainedModel):
         x = x.reshape(b, h, w, c).permute(0, 3, 1, 2)  # b, c, h, w
         vpt_feats = self.mask_patch_feats(x, regions)  # b, q, n, c
         vpt_feats = vpt_feats + self.position_embedding(self.position_ids)
+        
+        if self.config.use_mask_token:
+            mask_token = self.get_mask_token(regions)
+            vpt_feats = torch.cat([vpt_feats, mask_token], dim=2) # [b, q, n+1, c]
+
+        if self.config.use_projector:
+            vpt_feats = self.project(vpt_feats)
 
         if return_dict:
             result = dict(
