45cb948bbad291c7d28e5a74d9257185a07e7803
taiyan <yan.tai@sjtu.edu.cn>
2024-11-08 12:36:58 +0800

    we change the train_path

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
-NPROC_PER_NODE=8 xtuner train configs/train_stage2_nodecoder.py --deepspeed deepspeed_zero2 \
-	--work-dir /model/Aaronzhu/OkapiModel/vicuna_7b/stage2_ref/1107_lora_llm
+NPROC_PER_NODE=8 xtuner train configs/train_stage2_nodecoder.py --deepspeed deepspeed_zero2   --work-dir /checkpoints/Andychen/1108_unfreeze_3358
