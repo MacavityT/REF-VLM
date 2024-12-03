381bb638e0b7180adbb52f386474ccba7618efe2
taiyan <yan.tai@sjtu.edu.cn>
2024-12-03 09:23:38 +0800

    [Aaron] debug stage 1 success

--- scripts/train_debug.sh
+++ scripts/train_debug.sh
@@ -1,2 +1,2 @@
 
-python -m debugpy --wait-for-client --listen 127.0.0.1:5577 -m xtuner.tools.train configs/sketch_debug.py --deepspeed deepspeed_zero2
+python -m debugpy --wait-for-client --listen 127.0.0.1:5578 -m xtuner.tools.train configs/sketch_debug.py --deepspeed deepspeed_zero2
